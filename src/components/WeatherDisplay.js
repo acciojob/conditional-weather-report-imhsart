ba87cd01af3c86250185e3909c8fa1ac33b5b2c1
imhsart <nitinrana2519@gmail.com>
2026-05-24 13:33:18 +0530

    Assignment attempt 2

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,7 +3,7 @@ import React from "react";
 const WeatherDisplay = ({weatherData}) => {
   return (
     <div>
-      <p>Temperature : <span style={{color:weatherData.temperature > 20 ? 'red' : 'blue'}}>{weatherData.temperature}</span></p>
+      <p>Temperature: <span style={{color:weatherData.temperature > 20 ? 'red' : 'blue'}}>{weatherData.temperature}</span></p>
       <p>Conditions: {weatherData.conditions}</p>
     </div>
   )
